11839f3b4328ba05bf601bea84a4b7414b9f9e1a
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-11-13 23:13:44 +0800

    create Algorithm dir.

---
+++ Algorithm/__init__.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+# author: Alan-learner
+
+from Leetcode.Libs import *
+
+
+class Solution:
+    ac = accumulate()
+
+
+def main():
+    s = Solution()
+    res = s
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
