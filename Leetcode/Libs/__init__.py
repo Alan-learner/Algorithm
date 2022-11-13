e0331c13a176622c61c3867c13a1416ff2343c8e
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-11-13 23:09:19 +0800

    add Leetcode Libs dir.

---
+++ Leetcode/Libs/__init__.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+# author: Alan-learner
+import math
+from collections import deque, defaultdict, Counter
+from math import inf
+from typing import List, Optional
+from bisect import bisect_left, bisect_right
+from functools import reduce
+from heapq import heappush, heappop, heapreplace, heapify
+from math import comb
+from executing import cache
+from yarl import cache_clear
+
+from numpy import lcm
+from sortedcontainers import SortedList
+from itertools import permutations, combinations, accumulate
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
