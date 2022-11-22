2fcd5c58e106822e70afb00b3ca4ab1e18e94ccd
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-11-22 19:07:08 +0800

    update libs script.

--- Leetcode/Libs/__init__.py
+++ Leetcode/Libs/__init__.py
@@ -10,10 +10,15 @@ from heapq import heappush, heappop, heapreplace, heapify
 from math import comb
 from executing import cache
 from yarl import cache_clear
+from string import ascii_lowercase
 
 from numpy import lcm
 from sortedcontainers import SortedList
 from itertools import permutations, combinations, accumulate
+
+MOD = 10 ** 9 + 7
+lowbit = lambda x: x & -x
+inf = int(1e20)
 
 
 def main():
