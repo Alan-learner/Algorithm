07c39cffdcff9ad5a9becfa7b6e2f548bc9ad867
Alan_Huawei_Cup <XiangDongxu1024@163.com>
2022-12-14 20:50:12 +0800

    update leetcode libs template.

--- Leetcode/Libs/__init__.py
+++ Leetcode/Libs/__init__.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 # author: Alan-learner
+# 自带库
 import math
 from collections import deque, defaultdict, Counter
 from math import inf
@@ -11,15 +12,16 @@ from math import comb
 from executing import cache
 from yarl import cache_clear
 from string import ascii_lowercase
-
+# 第三方库
 from numpy import lcm
 from sortedcontainers import SortedList
 from itertools import permutations, combinations, accumulate
+# 拓展库
+from Data_Structure.unionfind_set.UnionFindSet import UnionFindSet
 
 MOD = 10 ** 9 + 7
 lowbit = lambda x_: x_ & -x_
 inf = int(1e20)
-
 
 def main():
     pass
